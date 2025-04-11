c58ed62105404f5d7f5fb730e6184b3a36ec1a88
dohauzi@gmail.com <dohauzi@gmail.com>
2025-04-11 13:08:57 +0900

    주석 추가

--- src/main/java/com/finance/dart/FinReportApplication.java
+++ src/main/java/com/finance/dart/FinReportApplication.java
@@ -1,13 +1,20 @@
 package com.finance.dart;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class FinReportApplication {
 
 	public static void main(String[] args) {
+		startupLogging();
 		SpringApplication.run(FinReportApplication.class, args);
 	}
 
+	public static void startupLogging() {
+		log.info("📝 JVM file.encoding = {}", System.getProperty("file.encoding"));
+		log.info("📝 JVM defaultCharset = {}", java.nio.charset.Charset.defaultCharset());
+	}
 }
