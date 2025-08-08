d3717c3883062ec374a81c9682c2e08d88f34535
dohauzi@gmail.com <dohauzi@gmail.com>
2025-08-08 09:28:12 +0900

    SEC API 목록 추가

--- src/main/java/com/finance/dart/api/abroad/enums/SecApiList.java
+++ src/main/java/com/finance/dart/api/abroad/enums/SecApiList.java
@@ -10,7 +10,7 @@ public enum SecApiList {
 
     /** 당기 영업이익 조회 **/
     OperatingIncomeLoss(
-            "XBRL companyconcept OperatingIncomeLoss API",
+            "XBRL 재무제표항목 OperatingIncomeLoss API",
             "https://data.sec.gov/api/xbrl/companyconcept/CIK{cik}/us-gaap/OperatingIncomeLoss.json",
             HttpMethod.GET,
             "https://www.sec.gov/search-filings/edgar-application-programming-interfaces",
@@ -19,11 +19,79 @@ public enum SecApiList {
 
     /** 유동자산 합계 조회 **/
     AssetsCurrent(
-            "XBRL companyconcept AssetsCurrent API",
+            "XBRL 재무제표항목 AssetsCurrent API",
             "https://data.sec.gov/api/xbrl/companyconcept/CIK{cik}/us-gaap/AssetsCurrent.json",
             HttpMethod.GET,
             "https://www.sec.gov/search-filings/edgar-application-programming-interfaces",
             "XBRL 유동자산 합계 조회 API"
+    ),
+
+    /** 유동부채 합계 조회 **/
+    LiabilitiesCurrent(
+            "XBRL 재무제표항목 LiabilitiesCurrent API",
+            "https://data.sec.gov/api/xbrl/companyconcept/CIK{cik}/us-gaap/LiabilitiesCurrent.json",
+            HttpMethod.GET,
+            "https://www.sec.gov/search-filings/edgar-application-programming-interfaces",
+            "XBRL 유동부채 합계 조회 API"
+    ),
+
+    /** ----------------------- 비유동자산내 투자자산 조회(4가지 조회, 없는 항목이 있을 수 있음) ----------------------- **/
+
+    /** 1. 장기매도 가능 증권 AvailableForSaleSecuritiesNoncurrent **/
+    AvailableForSaleSecuritiesNoncurrent(
+            "XBRL 재무제표항목 AvailableForSaleSecuritiesNoncurrent API",
+            "https://data.sec.gov/api/xbrl/companyconcept/CIK{cik}/us-gaap/AvailableForSaleSecuritiesNoncurrent.json",
+            HttpMethod.GET,
+            "https://www.sec.gov/search-filings/edgar-application-programming-interfaces",
+            "XBRL 장기매도 가능 증권 조회 API"
+    ),
+
+    /** 2. 지분법 투자 EquityMethodInvestments **/
+    LongTermInvestments(
+            "XBRL 지분법 투자 EquityMethodInvestments API",
+            "https://data.sec.gov/api/xbrl/companyconcept/CIK{cik}/us-gaap/EquityMethodInvestments.json",
+            HttpMethod.GET,
+            "https://www.sec.gov/search-filings/edgar-application-programming-interfaces",
+            "XBRL 지분법 투자 조회 API"
+    ),
+
+    /** 3. 기타 장기투자 OtherInvestments**/
+    OtherInvestments(
+            "XBRL 재무제표항목 OtherInvestments API",
+            "https://data.sec.gov/api/xbrl/companyconcept/CIK{cik}/us-gaap/OtherInvestments.json",
+            HttpMethod.GET,
+            "https://www.sec.gov/search-filings/edgar-application-programming-interfaces",
+            "XBRL 장기투자 조회 API"
+    ),
+
+    /** 4. 투자 및 대여금 InvestmentsAndAdvances**/
+    InvestmentsAndAdvances(
+            "XBRL 재무제표항목 InvestmentsAndAdvances API",
+            "https://data.sec.gov/api/xbrl/companyconcept/CIK{cik}/us-gaap/InvestmentsAndAdvances.json",
+            HttpMethod.GET,
+            "https://www.sec.gov/search-filings/edgar-application-programming-interfaces",
+            "XBRL 투자 및 대여금 조회 API"
+    ),
+
+    /** ---------------------------------------------- **/
+
+
+    /** 고정부채 합계 조회 **/
+    LiabilitiesNoncurrent(
+            "XBRL 재무제표항목 LiabilitiesNoncurrent API",
+            "https://data.sec.gov/api/xbrl/companyconcept/CIK{cik}/us-gaap/LiabilitiesNoncurrent.json",
+            HttpMethod.GET,
+            "https://www.sec.gov/search-filings/edgar-application-programming-interfaces",
+            "XBRL 고정부채 조회 API"
+    ),
+
+    /** 발행주식수 조회 **/
+    EntityCommonStockSharesOutstanding(
+            "XBRL 기업메타데이터 EntityCommonStockSharesOutstanding API",
+            "https://data.sec.gov/api/xbrl/companyconcept/CIK{cik}/dei/EntityCommonStockSharesOutstanding.json",
+            HttpMethod.GET,
+            "https://www.sec.gov/search-filings/edgar-application-programming-interfaces",
+            "XBRL 발행주식수 조회 API"
     ),
     ;
 
