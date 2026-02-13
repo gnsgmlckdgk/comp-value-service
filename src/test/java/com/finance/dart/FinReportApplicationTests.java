a02266eac4977fcf5470ea61b51d2786e31ce2a3
HwangChangHa <gnsgmlckdgk@naver.com>
2026-02-13 14:33:28 +0900

    테스트컨테이너 도커 환경 스팁 설정 추가

--- src/test/java/com/finance/dart/FinReportApplicationTests.java
+++ src/test/java/com/finance/dart/FinReportApplicationTests.java
@@ -13,7 +13,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest()
-@Testcontainers
+@Testcontainers(disabledWithoutDocker = true)
 @ActiveProfiles("test")
 class FinReportApplicationTests {
 
